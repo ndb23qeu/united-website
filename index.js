e7e319a166ec5cfd9d6fd456a77b14290b76f77e
vvk23tmu <hautonrandall@gmail.com>
2023-11-28 12:04:00 +0000

    Updated links to be displayed on index.html.

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ class HomeButton2
         this.title = title;
     }
 }
-const Button13 = new HomeButton1("","images/logo.jpg","Placeholder Logo","Climate Action","Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
-const Button14 = new HomeButton1("","images/logo.jpg","Placeholder Logo","Life Below Water","Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
-const Button15 = new HomeButton1("","images/logo.jpg","Placeholder Logo","Life On Land","Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
-const ButtonSignup = new HomeButton2("","Contact Us");
-const ButtonTeam = new HomeButton2("","About Us");
+const Button13 = new HomeButton1("page13.html","images/logo.jpg","Placeholder Logo","Climate Action","Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+const Button14 = new HomeButton1("page14.html","images/logo.jpg","Placeholder Logo","Life Below Water","Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+const Button15 = new HomeButton1("page15.html","images/logo.jpg","Placeholder Logo","Life On Land","Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+const ButtonSignup = new HomeButton2("signup.html","Contact Us");
+const ButtonTeam = new HomeButton2("team.html","About Us");
 
 const buttons1 = [];
 const buttons2 = [];

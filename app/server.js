3238664bd9a4cf5a3985db3372885b4bd2076418
talexanderuea <T.alexander@uea.ac.uk>
2023-11-29 02:57:24 +0000

    Signup form js & HTML + body parser

--- app/server.js
+++ app/server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express();
 const port = 3000
 
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const fs = require('fs')
+
 app.use(express.static('public'));
 
 
@@ -16,3 +21,20 @@ app.get('/', (req,res)=> {
 app.listen(port, () => {
     console.log(`My first app listening on port ${port}!`)
 });
+
+app.post('/signupHandler', jsonParser, (req,res) => {
+    console.log(process.cwd())
+    const data = req.body;
+    data.age=0
+    data.verified=false
+    const path = 'accounts/'+ data.email + '.json'
+    dataS = JSON.stringify(data)
+    fs.writeFile(path, dataS,(err) =>{
+        if (err) {
+            console.error('error!', err)
+            res.send(err)
+        } else {
+            res.send("yay")
+        }
+    })
+})

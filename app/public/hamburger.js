28653213bad65c740912715391c7a20db67af2f5
nbd23qeu <nbd23qeu@uea.ac.uk>
2023-12-09 15:25:03 +0000

    fixed flashing navbar when loading a new page

--- app/public/hamburger.js
+++ app/public/hamburger.js
@@ -2,9 +2,18 @@ function toggleNav()
 {
     let navbar = document.querySelector('.navBar');
     let classes = navbar.classList;
-    classes.toggle('showNav');
-    classes.toggle('hideNav')
-    console.log('hamburger click')
+    
+    if (classes.contains('showNav'))
+    {
+        classes.remove('showNav');
+        classes.add('hideNav');
+    }
+    else
+    {
+        classes.add('showNav');
+        classes.remove('hideNav');
+    }
+    console.log('hamburger click');
 }
 
 let hbmenu = document.querySelector('#hamburger');
